51e53d8c525f21c93c3039a82bc4fb45e5bc5040
Caleb Hessing <cckhh7@gmail.com>
2025-09-11 23:04:09 -0600

    ChessBoard: Row subclass created (is this stupid?)

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +11,52 @@ package chess;
  */
 public class ChessBoard {
 
+    private class Row {
+        private final ChessPiece[] squares; // TODO: this might not be final actually
+
+        Row() {
+            squares = new ChessPiece[8];
+            // TODO: do I have to initialize every element as null?
+        }
+
+        public ChessPiece getPieceAtColumn(int column) {
+            return squares[column - 1];
+        }
+
+        public void setPieceAtColumn(int column, ChessPiece piece) {
+            squares[column - 1] = new ChessPiece(piece.getTeamColor(), piece.getPieceType());
+        }
+
+        @Override
+        public String toString() {
+            return "Row{" +
+                    "squares=" + Arrays.toString(squares) +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            Row row = (Row) o;
+            return Objects.deepEquals(squares, row.squares);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(squares);
+        }
+    }
+
+    private final Row[] rows;
+
     public ChessBoard() {
-        
+        rows = new Row[8]; // initializes array of null Row references
+        // fill rows array w/ empty Rows:
+        for (int i = 0; i < rows.length; ++i) {
+            rows[i] = new Row();
+        }
     }
 
     /**
