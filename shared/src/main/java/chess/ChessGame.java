44c52972e2d097cc4e115f5668791625841630ad
Caleb Hessing <cckhh7@gmail.com>
2025-09-26 11:37:21 -0600

    Phase 1 begin fr

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,8 @@ package chess;
 
 import java.util.Collection;
 
+
+
 /**
  * For a class that can manage a chess game, making moves on a board
  * <p>
@@ -9,6 +11,9 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
+
+    private TeamColor teamTurn;
+    private ChessBoard board;
 
     public ChessGame() {
 
@@ -18,7 +23,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +32,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        teamTurn = team;
     }
 
     /**
@@ -96,7 +101,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -105,6 +110,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
