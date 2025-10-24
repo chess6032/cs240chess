b2510262bf1e22510232d94231197e6f9327f7c8
Caleb Hessing <cckhh7@gmail.com>
2025-10-23 18:34:40 -0600

    modified MemoryUserDAO so that it wasnt the stupidest thing in the world

--- server/src/main/java/dataaccess/MemoryDAO/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryDAO/MemoryUserDAO.java
@@ -1,26 +1,26 @@
 package dataaccess.MemoryDAO;
+
 import chess.model.UserData;
-
-
 import dataaccess.UserDAO;
-import kotlin.Pair;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class MemoryUserDAO implements UserDAO {
 
-    private final HashMap<String, Pair<String, String>> userDatas = new HashMap<>();
+    private final HashSet<UserData> users = new HashSet<>();
 
     @Override
     public UserData getUser(String username) {
-        if (userDatas.containsKey(username)) {
-            return new UserData(username, userDatas.get(username).component1(), userDatas.get(username).component2());
+        for (UserData userData : users) {
+            if (userData.username().equals(username)) {
+                return userData;
+            }
         }
         return null;
     }
 
     @Override
     public void createUser(UserData userData) {
-        userDatas.put(userData.username(), new Pair<String, String>(userData.password(), userData.email()));
+        users.add(userData);
     }
 }
