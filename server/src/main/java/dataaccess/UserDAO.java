893d58098b0bb816d15c6055c6eb3e64c7b833c7
Caleb Hessing <cckhh7@gmail.com>
2025-10-23 18:21:31 -0600

    tehnically working but I don't think userDAO is supposis supposed to work like that

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,15 +2,28 @@ package dataaccess;
 import chess.model.AuthData;
 import chess.model.UserData;
 
+
+import kotlin.Pair;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class UserDAO {
 
+    private final HashMap<String, Pair<String, String>> userDatas = new HashMap<>();
+    private final HashSet<AuthData> authTokens = new HashSet<>();
+
     public UserData getUser(String username) {
-        // FIXME: STUB
+        // FIXME: TEMPORARY
+        if (userDatas.containsKey(username)) {
+            return new UserData(username, userDatas.get(username).component1(), userDatas.get(username).component2());
+        }
         return null;
     }
 
     public void createUser(UserData userData) {
-        // FIXME: STUB
+        // FIXME: TEMPORARY
+        userDatas.put(userData.username(), new Pair<String, String>(userData.password(), userData.email()));
     }
 
     public void createAuth(AuthData authData) {
