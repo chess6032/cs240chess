fa189105a255290ed047142d56edd16d44cd6d2c
Caleb Hessing <cckhh7@gmail.com>
2025-10-25 16:26:27 -0600

    removed authservice.java

--- server/src/main/java/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import chess.model.http.LogoutRequest;
-import dataaccess.AuthDAO;
-import dataaccess.exceptions.AuthTokenNotFoundException;
-
-public interface AuthService {
-
-}
