cb091f6fea36905caea70834b7d9dfb7dbd51f65
Caleb Hessing <cckhh7@gmail.com>
2025-11-16 21:52:25 -0700

    i'm going to move model outside of the chess package bc that doesn't make sense having it there

--- client/src/main/java/servercomms/ServerFacade.java
+++ client/src/main/java/servercomms/ServerFacade.java
@@ -1,10 +1,39 @@
 package servercomms;
 
+import chess.model.*;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverURL;
 
-    private final int port;
+    public ServerFacade(String url) {
+        serverURL = url;
+    }
 
-    public ServerFacade(int port) {
-        this.port = port;
+    // SERVER COMMUNICATION METHODS
+
+    // TODO: implement these somehow
+    public void register() {
+
+    }
+    public void login() {
+
+    }
+    public void logout() {
+
+    }
+    public void createGame() {
+
+    }
+    public void listGames() {
+
+    }
+    public void joinGame() {
+
     }
 }
